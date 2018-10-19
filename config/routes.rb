ac048ae1955cdd4064d9f2029ce4cab937cf388e
Christopher Louviere <clouvier@tulane.edu>
2018-10-19 23:39:31 +0000

    Adding Rollify gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
   resources :courses
   get 'courses' => 'courses#index'
   get 'courses/new' => 'courses#new'
