1fba6d8945ffa65a493ca6e2be40c9bb3eac78a4
Christopher Louviere <clouvier@tulane.edu>
2018-10-19 23:13:22 +0000

    First commit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,25 @@
+class CoursesController < ApplicationController
+    def index
+        @courses = Course.all
+    end
+    
+    def new
+        @new_course = Course.new
+    end
+    
+  def create 
+  	new_course = Course.new(course_params) #Course.new(course_params) 
+  	if new_course.save 
+  	  flash[:notice] = "Course is added!"
+    	redirect_to root_path 
+  	else 
+  	  flash[:warning] = "Error entering data in db!"
+    	render 'new' 
+  	end 
+  end
+  
+  private
+  def course_params
+      params.require(:course).permit(:course_num, :department_id, :course_code, :section, :name, :location, :instructor, :days, :start_time, :end_time, :credits)
+  end
+end
