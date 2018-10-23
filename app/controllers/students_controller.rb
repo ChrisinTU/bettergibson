a43442c2dc7f67cbe74c0f6143cbfd7d58844d3d
Christopher Louviere <clouvier@tulane.edu>
2018-10-23 00:04:39 +0000

    Aded student MVC and pages

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+    def index
+        @students = Student.all
+    end
+    
+    def new
+        @new_student = Student.new
+    end
+    
+  def create 
+  	new_student = Student.new(student_params) #Course.new(course_params) 
+  	if new_student.save 
+  	  flash[:notice] = "Student is added!"
+    	redirect_to root_path 
+  	else 
+  	  flash[:warning] = "Error entering data in db!"
+    	render 'new' 
+  	end 
+  end
+  
+  private
+  def student_params
+      params.require(:student).permit(:major, :first_name, :last_name, :email, :student_id, :grad_year, :credits_taken)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019185448) do
+ActiveRecord::Schema.define(version: 20181022230451) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_num"
@@ -25,6 +24,18 @@ ActiveRecord::Schema.define(version: 20181019185448) do
     t.string   "start_time"
     t.string   "end_time"
     t.string   "credits"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "major"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "student_id"
+    t.string   "grad_year"
+    t.string   "credits_taken"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
