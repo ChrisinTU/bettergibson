f557670f14e59d8b4149d2e43647c1b9c3b97653
Siyang Hu <shu1@tulane.edu>
2018-11-02 13:51:58 +0000

    Edit the location of log in email

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,11 @@
 class Course < ActiveRecord::Base
 
     belongs_to :major
-
+    
+    # CSS: user input box size
+    validates :major_id,
+    presence: true,
+    length: {maximum: 255},
+    on: :create,
+    allow_nil: false
 end
