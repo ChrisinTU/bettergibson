85891179a1b1f95aa44f382cd7265ced79773c4a
eweathe <eweathe@tulane.edu>
2018-11-15 05:12:47 +0000

    Added new views for users and visitors. Also added an About page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 #require 'app/sanitizers/user_parameter_sanitizer'
 
 class ApplicationController < ActionController::Base
- 
+  def about
+    render "/about"
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
