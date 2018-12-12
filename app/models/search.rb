b303fcf5a55fda79053457114142384d18932f61
eweathe <eweathe@tulane.edu>
2018-12-12 18:30:38 +0000

    Debugging recommender

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,10 +8,6 @@ class Search < ApplicationRecord
     def find_courses
       courses = Course.order(:name)
       courses = courses.where("major_id = ? OR major_id = ? OR major_id = ? OR major_id = ?", primary_major, interest_1, interest_2, interest3) if primary_major.present?
-      # courses = courses.where(["major_id = ?", primary_major]) if primary_major.present?
-      # courses = courses.where(["major_id = ?", interest_1]) if interest_1.present?
-      # courses = courses.where(["major_id = ?", interest_2]) if interest_2.present?
-      # courses = courses.where(["major_id = ?", interest3]) if interest3.present?
       courses
     end
 end
