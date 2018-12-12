9c4243582bb2dd27ebeef085e1f0dd55d4620431
eweathe <eweathe@tulane.edu>
2018-12-12 23:04:32 +0000

    Added privacy policy to webpage

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   #get 'welcome/index'
   root 'welcome#index'
   get 'welcome/about' => 'welcome#about'
+  get 'welcome/privacy' => 'welcome#privacy'
   #FIX: https://www.railstutorial.org/book/filling_in_the_layout/*
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
