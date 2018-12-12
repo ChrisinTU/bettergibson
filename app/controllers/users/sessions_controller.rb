0a0bfeff9e6072d58618991774590f6cd161e66f
Noah Hendlish <nhendlis@tulane.edu>
2018-12-12 22:53:17 +0000

    fix logout with email acct

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,9 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+     super
+  end
 
   # POST /resource/sign_in
   # def create
@@ -14,15 +14,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+
 
   # protected
   def destroy
     super
-  	session[:user_id] = nil
-  	redirect_to root_path
+  	#session[:user_id] = nil
+  	#redirect_to root_path
   end
   
   def create
