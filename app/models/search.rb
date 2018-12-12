2b1dde5b4a598acdc200b3f278f109b3dec9fa35
eweathe <eweathe@tulane.edu>
2018-12-12 18:20:15 +0000

    testing recommender for heroku

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,20 +7,11 @@ class Search < ApplicationRecord
     
     def find_courses
       courses = Course.order(:name)
-      if !interest_1 || interest_1 == ''
-        interest_1 = '0'
-      end
-      if !interest_2 || interest_2 == ''
-        interest_2 = '0'
-      end
-      if !interest3 || interest3 == ''
-        interest3 = '0'
-      end
       courses = courses.where("major_id = ? OR major_id = ? OR major_id = ? OR major_id = ?", primary_major, interest_1, interest_2, interest3) if primary_major.present?
-      #courses = courses.where("major_id = ?", primary_major) if primary_major.present?
-      #courses = courses.where("major_id = ?", interest_1) if interest_1.present?
-      #courses = courses.where("major_id = ?", interest_2) if interest_2.present?
-      #courses = courses.where("major_id = ?", interest3) if interest3.present?
+      # courses = courses.where(["major_id = ?", primary_major]) if primary_major.present?
+      # courses = courses.where(["major_id = ?", interest_1]) if interest_1.present?
+      # courses = courses.where(["major_id = ?", interest_2]) if interest_2.present?
+      # courses = courses.where(["major_id = ?", interest3]) if interest3.present?
       courses
     end
 end
