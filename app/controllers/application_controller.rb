82f4243cab5cd04752e539006fc6fbe3c6dc83e4
Noah Hendlish <nhendlis@tulane.edu>
2018-12-13 00:30:38 +0000

    testing login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
-
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :major, :grad_year, :credits_taken, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,19 +8,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
   
-   def facebook # You need to implement the method below in your model (e.g. app/models/user.rb) 
+  def facebook # You need to implement the method below in your model (e.g. app/models/user.rb) 
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
-    #if @user.persisted? 
-      #sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    @user.save
+    #sign_in_and_redirect @user
+    if @user.persisted? 
+      sign_in_and_redirect @user #, :event => :authentication #this will throw if @user is not activated
       #set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    #else
-      #session["devise.facebook_data"] = request.env["omniauth.auth"]
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
       #redirect_to new_user_registration_url
-    #end
-    #def failure
-    #  redirect_to root_path
-    #end
+      redirect_to registration_url
+    end
+  end
+    
+  def failure
+    redirect_to root_path
   end
 end
 
